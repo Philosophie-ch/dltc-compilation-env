2583ecbe9f63d55248e4f107fb139731363578fd
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-10-28 04:55:23 +0200

    Working configuration for 01.11.23 demo

---
+++ copyeditor files/set-team-keys.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl https://raw.githubusercontent.com/alebg/dltc-compilation-env/alebg/issue7/copyeditor%20files/copyediting_team_public_keys -o /home/copyeditor/dltc-env-ssh/authorized_keys
--- dltc-env/23/build.sh
+++ dltc-env/23/build.sh
@@ -7,7 +7,7 @@ cat << EOF
 
 Showing help: [ -h | --help | help ]
 
-Without arguments, this script gets the current directory's name and the parent directory's name to build an image tag. It also uses the environment variable DOCKERUSR to build the tag, and prompts you for a docker username if DOCKERUSR is empty. Without arguments, this script then executes by default:
+Without arguments, this script gets the current directory's name and the parent directory's name to build an image tag. Without arguments, this script then executes by default:
 
 docker build -t ${image_tag} \\
              -f ${dockerfile_name} \\
@@ -25,10 +25,10 @@ OPTIONS
 -t and -f behave as in docker build, and its corresponding arguments override the default arguments above. Note that only one -t and one -f is supported (the ones declared last).
 
 -l(atest tag) adds an additional "latest" tag to docker build, constructed automatically as the default -t:
--t ${docker_user}${image_name}:latest
+-t ${image_name}:latest
 
 -p(rueba) deletes any -t or -l options, default or passed, and instead uses:
--t ${docker_user}${image_name}:test
+-t ${image_name}:test
 It preserves the default or passed -f
 
 EOF
@@ -41,7 +41,6 @@ Debug print to inspect variables:
 dockerfile_name = ${dockerfile_name}
 bc_f = ${bc_f}
 
-docker_user = ${docker_user}
 tag = ${tag}
 image_name = ${image_name}
 image_tag = ${image_tag}
@@ -63,28 +62,14 @@ dockerfile_name="Dockerfile"
 bc_f="-f ${dockerfile_name} "
 
 ### Default image tag
-# docker user
-#   docker_user will contain "/" unless the user leaves it empty when prompted
-echo "Note: A message about DOCKERUSR is shown ALWAYS (even if passing -h | --help | help). To skip the prompt if you see one and see help just press ENTER."
-echo ""
-if [ "${DOCKERUSR}" != "" ] ; then
-    echo "Using the DOCKERUSR (set to ${DOCKERUSR}) environment variable to automatically build the image tag (unless you passed the -t option, which overrides the automatic image tag)"
-    docker_user="${DOCKERUSR}/"
-else
-    echo "Environment variable DOCKERUSR not found, please enter your docker username or press ENTER to leave the image tag without username (e.g., you just don't want one, or passed the -t option, which overrides the automatic image tag): "
-    read d_user
-    [ "${d_user}" != "" ] \
-    && docker_user="${d_user}/" \
-    || docker_user="${d_user}"
-fi
-echo ""
 # current and parent directories' names by brute force
+# this is ugly, but works
 script_dir="$( dirname "$0" )"
 builtin cd $script_dir
 # By default, image_tag is based on how base directories are named, as in parent_dir:script_dir
 tag=${PWD##*/}
 image_name=$( builtin cd ../ && echo "${PWD##*/}" )
-image_tag="${docker_user}${image_name}:${tag}"
+image_tag="${image_name}:${tag}"
 bc_t="-t ${image_tag} "
 
 # Default latest tag is empty and flags are False
@@ -110,7 +95,7 @@ while getopts ${possible_option_arguments} arg; do
         f)  dockerfile_name="${OPTARG}"
             bc_f="-f ${dockerfile_name} " ;;
 
-        l)  bc_l="-t ${docker_user}${image_name}:latest " ;;
+        l)  bc_l="-t ${image_name}:latest " ;;
 
         p)  test_flag="True" ;;
 
@@ -119,7 +104,7 @@ while getopts ${possible_option_arguments} arg; do
 done
 
 [ "${test_flag}" == "True" ] \
-&& image_tag="${docker_user}${image_name}:test" \
+&& image_tag="${image_name}:test" \
 && bc_t="-t ${image_tag} " \
 && bc_l=
 
