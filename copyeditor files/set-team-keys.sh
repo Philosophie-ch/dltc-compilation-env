90144482ad374165c31b40895bce4c1d9b1a4de5
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-11-08 11:39:41 +0100

    Modified the ssh-server beta version to a version meant to be run locally

--- copyeditor files/set-team-keys.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://raw.githubusercontent.com/alebg/dltc-compilation-env/alebg/issue7/copyeditor%20files/copyediting_team_public_keys -o /home/copyeditor/dltc-env-ssh/authorized_keys
